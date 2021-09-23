970fb90efffe216b56cc7155ca88b47036df8e78
madi <madimaecooley@gmail.com>
2021-09-22 21:34:15 -0600

    basic decision tree

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -18,42 +18,46 @@ import utils.utils as utils
 
 class Tree():
     def __init__(self):
-        self.root = Node()
-        self.adjacency_list = [self.root]
+        self.adjacency_list = []
     
-    def add_node(self, parent=None, node=None, edge_attr=None):
-        #print('\n*** adding node: parent=', parent)
-        if parent is None:
-           parent = len(self.adjacency_list)-1
-        
-        self.adjacency_list.append(node)
-        index = len(self.adjacency_list)-1
-
-        #print('parent=', parent, ' child=', index)
-        edge = Edge(index, attr_value=edge_attr)
-        #print(edge.child_node, edge.attr_value)
-        self.adjacency_list[parent].children.append(edge)
-        #print('done adding node=', index, '***\n')    
-        
-        return index
+    def add_node(self, node=None):
+        if node.index is None:
+            self.adjacency_list.append(node)
+            node.index = len(self.adjacency_list)-1
+        return node.index
+    
+    def add_edge(self, child_index, parent_index, edge_attr=None):
+        edge = Edge(child_index, attr_value=edge_attr)
+        self.adjacency_list[parent_index].children.append(edge)
     
     def print_tree(self):
         print('\n--------------------------------------tree:')
         i=0
         for node in self.adjacency_list:
-            print(i, node.splitting_attr, node.leaf_label, '\n  childs: [ ', end='')
-            for child in node.children:
-                print('(', child.child_node, child.attr_value, ') , ', end='')
-            print(' ]\n')
+            print(i, ': ', end='')
+            node.print_node()
+            print()
             i+=1
         print('--------------------------------------')
 
 
 class Node():
     def __init__(self, splitting_attr=None, leaf_label=None):
+        self.index = None
         self.splitting_attr = splitting_attr        # splitting attr value 
         self.leaf_label = leaf_label                # leaf_label
         self.children = []
+    
+    def print_node(self):
+        print('___________________')
+        print('node index= ', self.index)
+        #print('parent node = ', self.parent)
+        print('splitting atrr=', self.splitting_attr)
+        print('leaf label=', self.leaf_label)
+        print('children: [', end='')
+        for child in self.children:
+            print('(', child.child_node, child.attr_value, ') , ', end='')
+        print(' ]\n______________________\n')
         
         
 class Edge():
@@ -89,29 +93,21 @@ def gini_index():
 
 def ID3(max_depth, S, attrs, label, edge_attr=None):
     global tree, attributes
-    print('\n\n____________________________________________________ID3')
-    #tree.print_tree()
-    print('S: ', S)
-    print('attrs: ', attrs)
-    print('labels: ', label)
-    print('edge attr: ', edge_attr)
-    
+
     if len(set(label)) == 1:                    # return leaf node w. label
         leaf = Node(leaf_label=label[0])
-        print('______________________________________________return leaf 1')
         return leaf
     
     elif len(attrs) < 1:                        # return leaf node w. most common label
         leaf = Node(leaf_label=get_majority_label(label))
-        print('______________________________________________return leaf 2')
         return leaf
     
     else:
         # find attribute that best splits S
         A, A_values = get_best_attribute(attrs)
         root_node = Node(splitting_attr=A)
-        root = tree.add_node(node=root_node, edge_attr=edge_attr)    # create root node for tree
-    
+        root_index = tree.add_node(node=root_node)          # create root node for tree
+        
         for v in A_values:                                  # add new tree branch corresponding to A=v            
             # Sv be subset of examples in S w. A=v
             inds = [i for i in range(len(S)) if S[i][A]==v]            
@@ -120,11 +116,13 @@ def ID3(max_depth, S, attrs, label, edge_attr=None):
             
             if len(Sv) < 1:                     # add leaf node w. most common value of label in S 
                 leaf_node = Node(leaf_label=get_majority_label(label))
-                leaf = tree.add_node(parent=root, node=leaf_node, edge_attr=v)
+                leaf_index = tree.add_node(node=leaf_node)
+                tree.add_edge(leaf_index, root_index, edge_attr=v)
             else:                               # below this branch add the subtree ID3(Sv, attrs-{A}, label)
                 node = ID3(max_depth, Sv, copy.deepcopy(attrs), label_v, edge_attr=v)
-                subtree = tree.add_node(parent=root, node=node, edge_attr=v)
-                
+                subtree_index = tree.add_node(node=node)
+                tree.add_edge(subtree_index, root_index, edge_attr=v)
+        
         return root_node
 
 
@@ -138,12 +136,12 @@ def problem_two():
     x_test, y_test = utils.readin_dat('decision_tree/data/car/', 'test.csv')
     
     global attributes
-    #attributes = [{'buying'      : set()}, 
-                  #{'maint'       : set()}, 
-                  #{'doors'       : set()}, 
-                  #{'persons'     : set()},
-                  #{'lug_boot'    : set()},
-                  #{'safety'      : set()}]
+    attributes = [{0 : set()}, # 'buying'
+                  {1 : set()}, # 'maint' 
+                  {2 : set()}, # 'doors'
+                  {3 : set()}, # 'persons'
+                  {4 : set()}, # 'lug_boot'
+                  {5 : set()}] # 'safety'
                   
     attributes = [{0 : set()}, {1 : set()}]
     
