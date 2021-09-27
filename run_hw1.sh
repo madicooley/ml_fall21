c6362a988f72048a6aba19d813bafcaa1e57b0a6
MADISON M COOLEY <u1266040@lab1-31.eng.utah.edu>
2021-09-26 23:42:38 -0600

    adding bash file

--- run_hw1.sh
+++ run_hw1.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-python decision_tree/decision_tree.py 
+#!/bin/bash
+
+## /usr/local/bin/conda create -n hw_env python=3.6 anaconda
+## /usr/local/bin/conda activate hw1_env
+
+cd decision_tree/
+python decision_tree.py 
